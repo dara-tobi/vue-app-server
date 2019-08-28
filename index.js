366f6bc29d5dc6d873ed8f7bb7fde5f560a36d1f
Dara Oladosu <daradosu@gmail.com>
2019-08-28 11:35:11 +0100

    setting up server index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const app = express();
+const api = require('./api');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.set('port',  (process.env.PORT || 8081));
+app.use('/api', api);
+app.use(express.static('static'))
+app.use(morgan('dev'))
+app.use((req, res, next) => {
+	const err = new Error('not found');
+	err.status = 404;
+	res.json(err);
+});
+
+
+mongoose.connect(process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/venom');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('connected to db');
+	app.listen(app.get('port'), () => {
+		console.log('APP listening on PORT', app.get('port')); 
+	});
+});
